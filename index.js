3952a87f2284b97ca024b6a4a2de79f3741af3fc
mickyash <sharmayash2862@gmail.com>
2023-08-26 16:25:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+
+
+const addInput = document.querySelector('#addlist');
+const addbtn = document.querySelector('#submit');
+const ul = document.querySelector('ul');
+const li = document.createElement('li')
+
+
+
+function addlists(){
+    if(addInput.value === ' '){
+        
+    }
+    else{
+        const ul = document.querySelector('ul');
+        const li = document.createElement('li')
+        li.innerHTML = addInput.value;
+        addInput.value = ' ' ;
+        ul.appendChild(li);
+        apenddelete(li);
+
+        saveToLocalStorage();
+       
+    }
+}
+
+
+
+function saveToLocalStorage(){
+    const listItmes = ul.querySelectorAll('li');
+    const itemsArray = [];
+
+    listItmes.forEach(item =>{
+        itemsArray.push(item.textContent.trim());
+    })
+    localStorage.setItem('todoItems', JSON.stringify(itemsArray));
+   
+}
+
+
+addbtn.addEventListener('click', ()=>{
+    addlists();
+})
+
+addInput.addEventListener('keyup',(e)=>{
+    if(e.which === 13){
+        addlists();
+    }
+})
+
+
+
+
+
+
+function apenddelete(li) {//append the delete button to every list 
+    var deletebtn = document.createElement('button');
+    deletebtn.textContent = 'Delete'; 
+    li.appendChild(deletebtn);
+    deletebtn.addEventListener('click' ,()=>{
+        ul.removeChild(li);
+
+        const savedItems = localStorage.getItem('todoItems');
+        if (savedItems) {
+            const itemsArray = JSON.parse(savedItems);
+            const itemIndex = itemsArray.indexOf(li.textContent.trim()); // Trim before comparing
+            if (itemIndex !== -1) {
+                itemsArray.splice(itemIndex, 1);
+                localStorage.setItem('todoItems', JSON.stringify(itemsArray));
+                location.reload()
+            }
+        }
+        
+    });
+}
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedItems = localStorage.getItem('todoItems');
+    if (savedItems) {
+        const itemsArray = JSON.parse(savedItems);
+        itemsArray.forEach(itemText => {
+            const li = document.createElement('li');
+            li.textContent = itemText;
+            ul.appendChild(li);
+            apenddelete(li);
+           
+        });
+        
+    }
+});
